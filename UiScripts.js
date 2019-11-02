80834e6030e100dd3d413b07d7142ffbb405e5e0
Kendrick Bong <Kendrickbong1996@gmail.com>
2019-11-02 09:59:01 +0800

    Added close menu UI

--- UiScripts.js
+++ UiScripts.js
@@ -1,3 +1,27 @@
 function openMenu() {
   document.querySelector("ion-menu-controller").open();
 }
+
+function homePage() {
+  document.getElementById("pageTitle").innerHTML =
+    "<ion-title>Home Page</ion-title>";
+  document.querySelector("ion-menu-controller").close();
+}
+
+function navPage() {
+  document.getElementById("pageTitle").innerHTML =
+    "<ion-title>Navigation Page</ion-title>";
+  document.querySelector("ion-menu-controller").close();
+}
+
+function itePage() {
+  document.getElementById("pageTitle").innerHTML =
+    "<ion-title>Itinerary Page</ion-title>";
+  document.querySelector("ion-menu-controller").close();
+}
+
+function chatPage() {
+  document.getElementById("pageTitle").innerHTML =
+    "<ion-title>Live Chat Page</ion-title>";
+  document.querySelector("ion-menu-controller").close();
+}
